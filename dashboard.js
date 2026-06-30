93636de13ffda0fcf931a2ab679405e997c59273
Avni2007 <dewanavni488@gmail.com>
2026-06-30 12:11:28 +0530

    add favorite pairs

--- dashboard.js
+++ dashboard.js
@@ -2,9 +2,14 @@ const amount = document.getElementById("amount");
 const crypto = document.getElementById("crypto");
 const currency = document.getElementById("currency");
 const convertBtn = document.getElementById("convertBtn");
+const saveBtn = document.getElementById("saveBtn");
 const result = document.getElementById("result");
+const favorites = document.getElementById("favorites");
 
 convertBtn.addEventListener("click", getPrice);
+saveBtn.addEventListener("click", saveFavorite);
+
+displayFavorites();
 
 async function getPrice() {
 
@@ -24,12 +29,11 @@ async function getPrice() {
         const data = await response.json();
 
         const price = data[coin][curr];
-
         const total = amount.value * price;
 
         result.innerHTML = `
             <h3>Conversion Result</h3>
-            <p>${amount.value} ${coin.toUpperCase()}</p>
+            <p>${amount.value} ${coin}</p>
             <p>= ${total.toFixed(2)} ${curr.toUpperCase()}</p>
         `;
 
@@ -38,5 +42,36 @@ async function getPrice() {
         result.innerHTML = "Unable to fetch data.";
 
     }
+
+}
+
+function saveFavorite() {
+
+    const pair = crypto.value.toUpperCase() + " → " + currency.value.toUpperCase();
+
+    let favoritePairs = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    if (!favoritePairs.includes(pair)) {
+        favoritePairs.push(pair);
+        localStorage.setItem("favorites", JSON.stringify(favoritePairs));
+    }
+
+    displayFavorites();
+
+}
+
+function displayFavorites() {
+
+    favorites.innerHTML = "";
+
+    let favoritePairs = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    favoritePairs.forEach(function(pair) {
+
+        const li = document.createElement("li");
+        li.textContent = pair;
+        favorites.appendChild(li);
+
+    });
 
 }
