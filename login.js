3a206a5be470e323a7e6bc2d77c3b960a317b33b
Avni2007 <dewanavni488@gmail.com>
2026-06-30 12:20:07 +0530

    login authentication

--- login.js
+++ login.js
@@ -1,39 +1,45 @@
 const loginForm = document.getElementById("loginForm");
 const errorMessage = document.getElementById("errorMessage");
 
-loginForm.addEventListener("submit", async function(event) {
+loginForm.addEventListener("submit", async function(event){
 
     event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
 
-    try {
+    try{
 
         const response = await fetch("users.json");
         const users = await response.json();
 
-        let isValidUser = false;
+        let validUser = false;
 
-        for (let i = 0; i < users.length; i++) {
+        for(let i = 0; i < users.length; i++){
 
-            if (
-                users[i].username === username &&
-                users[i].password === password
-            ) {
-                isValidUser = true;
+            if(users[i].username === username && users[i].password === password){
+                validUser = true;
                 break;
             }
+
         }
 
-        if (isValidUser) {
+        if(validUser){
+
             window.location.href = "dashboard.html";
-        } else {
+
+        }else{
+
             errorMessage.textContent = "Invalid Username or Password";
+
         }
 
-    } catch (error) {
+    }
+
+    catch(error){
+
         errorMessage.textContent = "Unable to load users.";
+
     }
 
 });
